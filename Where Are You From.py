a574825ae7830b6eee67de6cbf5c554c79004de7
waywinnie <ralynnyilasat@gmail.com>
2025-04-01 21:33:34 -0500

    Add files via upload

---
+++ Where Are You From.py
@@ -0,0 +1,18 @@
+# This program asks the user about which state they're from
+
+# Ask for the user's name
+name = input("What is your name? ")
+
+# Ask which state they are from
+home_state = input(f"Hi, {name}! Which state are you from? ")
+
+# Ask what their favorite thing about their state is
+favorite_thing = input(f"What is your favorite thing about {home_state}? ")
+
+# Ask if they would like to visit another state
+visit_state = input = "Which state have you not been to but would like to visit? "
+
+# Display the information
+print(f"\nThanks for sharing, {name}!")
+print(f"You're from {home_state}, and your favorite thing is {favorite_thing}.")
+print(f"You'd love to visit {visit_state} someday. I hope you can visit soon!")
